a0daeff194accba481d6ebdf68f27dc6d349a80c
Adrian-898 <adriantorres898@gmail.com>
2024-10-21 18:12:26 -0300

    Map Update

--- app/(tabs)/mapa.tsx
+++ app/(tabs)/mapa.tsx
@@ -1,8 +1,16 @@
 import React, { useState, useEffect, useCallback } from "react";
 import * as Location from "expo-location";
-import { SafeAreaView, useWindowDimensions, Alert, Text } from "react-native";
+import {
+  SafeAreaView,
+  useWindowDimensions,
+  Alert,
+  Text,
+  View,
+  StyleSheet,
+} from "react-native";
 import MapView, { Callout, Marker } from "react-native-maps";
 import { StatusBar } from "react-native";
+import Icon from "@expo/vector-icons/FontAwesome";
 
 // interfaz para almacenar y utilizar informacion obtenida de las funciones de expo-location
 interface Location {
@@ -50,16 +58,12 @@ const useGetCurrentLocation = () => {
       const current: Location = {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05,
+        latitudeDelta: 0.04,
+        longitudeDelta: 0.04,
       };
       setOrigin(current);
     } catch (error) {
       console.log(error);
-      Alert.alert(
-        "Error:",
-        "No se pudo obtener tu ubicación actual, verifica que el botón de acceso está activado"
-      );
     }
   }, []);
 
@@ -75,33 +79,44 @@ const Mapa = () => {
   const windowWidth = useWindowDimensions().width;
   const windowHeight = useWindowDimensions().height;
 
+  const [permissionGranted, setPermissionGranted] = useState<boolean>(false);
+  const [locationEnabled, setLocationEnabled] = useState<boolean>(false);
+
   // Funcion que solicita permisos de ubicacion al usuario
   const getLocationPermission = async () => {
     try {
       let { status } = await Location.requestForegroundPermissionsAsync();
-
-      if (status !== "granted") {
+      setPermissionGranted(status === "granted");
+      if (status === "granted") {
+        getCurrentLocation();
+        checkLocationServices();
+      } else {
         Alert.alert(
           "Atención:",
           "Se ha negado el permiso para acceder a la ubicación"
         );
-        return;
       }
-
-      getCurrentLocation();
     } catch (error) {
       console.log(error);
     }
   };
 
+  const checkLocationServices = async () => {
+    const servicesEnabled = await Location.hasServicesEnabledAsync();
+    setLocationEnabled(servicesEnabled);
+  };
+
   // se piden permisos de ubicacion al iniciar la app
   useEffect(() => {
-    try {
-      getLocationPermission();
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+    getLocationPermission();
+    const interval = setInterval(() => {
+      if (permissionGranted) {
+        checkLocationServices();
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [permissionGranted]);
 
   // componente Pin en el mapa
   const MarkerComponent = ({ coord }: { coord: Lugar }) => (
@@ -139,12 +154,16 @@ const Mapa = () => {
         showsIndoors={false}
         showsBuildings={false}
         showsScale={false}
-        region={{
-          latitude: origin.latitude,
-          longitude: origin.longitude,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
+        showsIndoorLevelPicker={false}
+        showsTraffic={false}
+        mapPadding={{ bottom: 50, left: 0, right: 0, top: 0 }}
+        initialRegion={{
+          latitude: 10.597032,
+          longitude: -66.930431,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
         }}
+        region={origin}
       >
         {
           // itera el arreglo lugares y renderiza un pin para cada instancia existente
@@ -153,8 +172,52 @@ const Mapa = () => {
           ))
         }
       </MapView>
+      {permissionGranted && !locationEnabled && (
+        <View style={styles.alertContainer}>
+          <Icon
+            name="exclamation"
+            color={"red"}
+            size={80}
+            style={styles.alertIcon}
+          />
+          <Text style={styles.alertMessage}>
+            El acceso a la ubicación está desactivado, actívalo en la
+            configuración de tu dispositivo.
+          </Text>
+        </View>
+      )}
+      {!permissionGranted && (
+        <View style={styles.alertContainer}>
+          <Icon
+            name="exclamation"
+            color={"red"}
+            size={80}
+            style={styles.alertIcon}
+          />
+          <Text style={styles.alertMessage} adjustsFontSizeToFit={true}>
+            Los permisos de ubicación fueron negados, ve a la configuración de
+            la App para otorgar los permisos
+          </Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  alertContainer: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    position: "absolute",
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 10,
+    bottom: 50,
+    maxWidth: "95%",
+    maxHeight: "30%",
+  },
+  alertIcon: { margin: 10 },
+  alertMessage: { color: "black", fontSize: 18, flex: 1 },
+});
 
 export default Mapa;
