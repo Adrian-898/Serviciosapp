dad3ad52a6aad156123ce1b5c6cb8ee45de9c5a4
Adrian-898 <adriantorres898@gmail.com>
2025-02-04 15:31:42 -0400

    Added loading state

--- components/LoadingState.tsx
+++ components/LoadingState.tsx
@@ -1,11 +1,15 @@
 import { ActivityIndicator, StyleSheet } from 'react-native';
 import { ThemedView } from './ThemedView';
+import { ThemedText } from './ThemedText';
 
 // Indicador de Actividad (Cargando...)
-const Loading = () => {
+const LoadingState = () => {
 	return (
 		<ThemedView style={styles.container}>
 			<ActivityIndicator color={'#001f7e'} size={50} />
+			<ThemedText type='subtitle' style={styles.message}>
+				Cargando...
+			</ThemedText>
 		</ThemedView>
 	);
 };
@@ -15,6 +19,10 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
+	message: {
+		textAlign: 'center',
+		marginTop: 20,
+	},
 });
 
-export default Loading;
+export default LoadingState;
--- components/NetworkAwareReset.tsx
+++ components/NetworkAwareReset.tsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { useNetInfo } from '@react-native-community/netinfo';
 import { ThemedView } from './ThemedView';
-import Loading from './LoadingState';
+import LoadingState from './LoadingState';
 
 /*
 children: componente a remontar
 onReconnect: callback opcional (sirve para ejecutar cualquier accion al recuperar la conexion)
-(!! en caso de usar onReconnect SE DEBE DEFINIR dentro de un USECALLBACK en el componente padre para evitar re-renderizados innecesarios !!)
+(!!! en caso de usar onReconnect SE DEBE DEFINIR dentro de un USECALLBACK en el componente padre para evitar re-renderizados innecesarios !!!)
 */
 type NetworkAwareResetProps = {
 	children: React.ReactNode;
@@ -17,11 +17,13 @@ type NetworkAwareResetProps = {
 const NetworkAwareReset = ({ children, onReconnect }: NetworkAwareResetProps) => {
 	// el key vuelve a montar el componente (children) cuando el estado de la red pasa a conectado nuevamente
 	const [key, setKey] = useState(0);
+
 	// calcula el estado actual de la conexion a internet
-	const isOnline = useNetInfo().isConnected && !!useNetInfo().isInternetReachable;
+	const netInfo = useNetInfo();
+	const isOnline = netInfo.isInternetReachable && netInfo.isConnected;
 
 	useEffect(() => {
-		if (isOnline) {
+		if (isOnline === true) {
 			// al reconectarse se vuelve a montar el componente (children) por el cambio en el valor de key
 			setKey((prev) => prev + 1);
 
@@ -30,7 +32,7 @@ const NetworkAwareReset = ({ children, onReconnect }: NetworkAwareResetProps) =>
 		}
 	}, [isOnline, onReconnect]);
 
-	if (!isOnline) return <Loading />;
+	if (!netInfo.isInternetReachable) return <LoadingState />;
 
 	return (
 		<ThemedView key={key} style={styles.container}>
